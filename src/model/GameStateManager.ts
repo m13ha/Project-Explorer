6680d0c2022f91388a7caf49d18d06f7e7b4b8f0
Centipede <irwinakoko@gmail.com>
2023-06-17 10:04:00 +0100

    minor update

--- src/model/GameStateManager.ts
+++ src/model/GameStateManager.ts
@@ -1,17 +1,18 @@
 // Handles all responses relating to gameplay - from setting up gameplay resources to responding to user inputs
-import { GameBoard } from "./GameBoard"
+
+import { Gameboard } from "./Gameboard";
 
 
 export class GameStateManager {
 
-    private gameBoard: GameBoard;
+    private gameBoard: Gameboard
     // How the sum of each row should look when the puzzle is arranged
-    public completionState: number[]
+    private completionState: number[]
 
     // Things we want to be done only once throughout the game
     constructor() {
         
-        this.gameBoard = new GameBoard();
+        this.gameBoard = new Gameboard();
         this.init();
     }
     
@@ -21,12 +22,12 @@ export class GameStateManager {
         this.completionState = new Array(this.gameBoard.getCompletionState().length);
         this.completionState[0] = 0;   // First row sums to 0
 
-        let nth = this.completionState.length - 1;     
-        
-        // Compute the sums of the subsequent rows
-        for (let i = 1; i <= nth; ++i) {
-            let initial = (i - 1) * nth + 1;
-            this.completionState[i] = (2 * initial + (nth - 1)) * nth /2;
+        let n = this.completionState.length - 1;         
+        // Compute the sums of the subsequent rows by computing the sum of consecutive integers, where "initial" is the first integer
+        for (let i = 1; i <= n; ++i) {
+            let initial = (i - 1) * n + 1;
+            // sum = (2a + (n-1)d) * n/2 -> sum of the numbers in an arithmetric progression sequence
+            this.completionState[i] = (2 * initial+ (n - 1)) * n / 2;    // a = initial, d = 1
         }
     }
 
@@ -37,7 +38,7 @@ export class GameStateManager {
     }
 
 
-    public isGameComplete(): boolean {
+    isGameComplete(): boolean {
         let gameBoardCompletionState = this.gameBoard.getCompletionState();
 
         for (let i = 0; i < this.completionState.length; ++i)
@@ -47,7 +48,12 @@ export class GameStateManager {
         return true;
     }
 
-    public getGameBoard(): GameBoard {
-        return this.gameBoard;
+
+    getCompletionState(): number[] {
+        return this.completionState;
+    }
+
+    getGameboardAsArray(): number[][] {
+        return this.gameBoard.getInternalBoard();
     }
 }
--- src/model/Randomizer.ts
+++ src/model/Randomizer.ts
@@ -1,7 +1,8 @@
-// Stores and randomizes integers 
+// Stores and randomizes integers. Disposable - It is created in every new game
 
 export class Randomizer {
 
+    // The numbers assigned the each tile on the grid
     private array: number[];
     private index: number;
 
@@ -9,21 +10,23 @@ export class Randomizer {
     constructor(length: number) {
         this.array = new Array(length);
         for (let i = 0; i < length; ++i)
+            // Assign numbers form 1 up to length
             this.array[i] = i + 1;
 
         this.index = 0;
-        this.shuffle();
+        this.randomize();
     }
 
 
-    private shuffle(): void {
+    private randomize(): void {
         for (let i = this.array.length - 1; i > 0; i--) {
+            // Generate an index between 0 up to i. The element at that index, j,  will be swapped with the element at index i
             const j = Math.floor(Math.random() * (i + 1));
             [this.array[i], this.array[j]] = [this.array[j], this.array[i]];
         }
     }
 
-
+    // Returns the current element the index points to, then increments the index
     next(): number {
         return this.array[this.index < this.array.length? this.index++ : this.array.length - 1];
     }
